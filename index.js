afd83a149efcd68c4851a572054fcc35de526c4f
Jonathan Shine <jonathanhshine@gmail.com>
2021-05-25 11:53:50 +0200

    added comments explaining the code for final presentation

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// The Cashier Problem
+
+// This function is designed to tell a cashier the change to give back in the least amount of bills and coins possible (with a theoretically endless supply of money in the cash register - must be nice!)
+
+// The function takes two parameters, the price of the item(s) and the amount of money given as payment
+
 function changeToGiveBack(price, paid) {
+    // We first need to find the total amount of money to be given back a change, which we find by subtracting the price of purchase from the amount of money given
     change = paid - price;
+
+    // The object 'billsBack' acts as a template for the denominations of coins and bills in Euros that are available to give back. Each denomination is initially set to 0, which can then be incremented by one depending on conditional statements later in the function
     let billsBack = 
     {'200 Euro': 0,
     '100 Euro': 0,
@@ -15,6 +24,10 @@ function changeToGiveBack(price, paid) {
     '0.02 Cent': 0,
     '0.01 Cent': 0};
 
+    // To start determining which bills should be returned, I used while statements. We will target the variable 'change' which tells us the total amount of money to be paid back. The value of 'change' will then be modified if the given condition is met. We will start with the biggest denomination (200 Euro bill) checking if the amount of 'change' is greater than the given denomination, and if so subtracting the denomination as many times as possible from 'change' (as long as the while condition holds true). Eventually the value of 'change' will be below 200, meaning we can no longer subtract 200 from 'change,' so the function moves on to the next denomination, the 100 Euro bill, repeating the entire process instead using the new denomination. This goes on through all the denominations down to 0.01 Cents.
+
+    // *** As Javascript has some funny issues with small numbers (when they have several decimal places), I had to adjust the conditions in the while statements to use the number one decimal place under the denominations actual value. This ensures that the code is running properly, maybe not the best workaround, but it works ***
+    
     while (change > 199.99) {
         change -= 200;
         billsBack['200 Euro'] += 1;  
@@ -68,20 +81,35 @@ function changeToGiveBack(price, paid) {
         billsBack['0.01 Cent'] += 1;
     };
 
-
+    
+    // The while statements will modify the template to include the number of the bills that need to be given back, but it also includes denominations that are not needed. This next block of code is designed to log only the bills needed to be returned in an easily read message logged to the console.
+    
     if (change === 0) {
+        // The first statement checks if the exact amount was paid, therefore requiring no change to be given back
         console.log('The exact amount was paid');
+    
     } else if (price > paid) {
+        // This else/if will tell the cashier if the customer didn't give enough money for the purchase
         console.log("They did not pay enough money");
+    
     } else {
+        
+        // In this last block of code we create the message to be logged as a string with the variable 'statement'
         let statement = "You must give back ";
+        
         for (let key in billsBack) {
-            if (billsBack[key] > 0) {
+           // This for/in loop iterates through the object 'billsBack' to look at the number associated with each denomination. 
+            
+           if (billsBack[key] > 0) {
+                // This if statement, paired with the enclosing for/in loop, will check the value of each of the keys of the object 'billsBack.' If the value is greater than 0 it will add and assign a template literal of the key:value pair to the variable 'statement'
                 statement += `'${[key]}' X ${billsBack[key]}, `
-            }
-        } console.log(statement.slice(0, -2));
-    }
+            };
+        }; 
+        
+        // This last line will log the modified variable 'statement' to the console, but it uses the slice method for a little cosmetic upgrade, trimming off one character of whitespace and the comma at the end of the 'statement' string
+        console.log(statement.slice(0, -2));
+    };
 };
 
 
-changeToGiveBack(3.75, 50);
+changeToGiveBack(3.75, 100);
